cfd3120db19afbdd47e7ad5249a13a9527583609
Eduardokorb2023 <148217028+Eduardokorb2023@users.noreply.github.com>
2024-09-25 18:59:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao('desconhecido',
+     ' como sonegar imposto', 
+    'se você olho provavelmente você que se preso.'
+);
+criaCartao(
+    'ciências', 
+    'quantos dias podemos ficar sem água?', 
+    'cerca de dois a quatro dias.'
+);
+criaCartao(
+    'espanhol',
+    'Como se diz "JACARÉ" em espanhol?',
+    'caimán'
+    )
+
+    criaCartao('charada',
+        'sobe e desce, mas nunca se mexe', 
+       'a escada.'
+   );
+   criaCartao(
+       'Geografia', 
+       'qual o maior país do mundo?', 
+       'rússia.'
+   );
+   criaCartao(
+       'música',
+       'Como música começa com: amogo faz o pix...',
+       'o nome da musica é amigo faz o pix, e você pode olha ela no final do site '
+       
+       )
